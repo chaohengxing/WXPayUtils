169d76a4e677130b2f276e9893638f45dc8f609f
chaohengxing <hengxingchao@163.com>
2018-05-02 19:44:04 +0800

    添加WXPayEntryActivity文件

---
+++ app/src/main/java/com/chx/wxpay/wxpayutils/wxapi/WXPayEntryActivity.java
@@ -0,0 +1,64 @@
+package com.chx.wxpay.wxpayutils.wxapi;
+
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.tencent.mm.opensdk.constants.ConstantsAPI;
+import com.tencent.mm.opensdk.modelbase.BaseReq;
+import com.tencent.mm.opensdk.modelbase.BaseResp;
+import com.tencent.mm.opensdk.openapi.IWXAPI;
+import com.tencent.mm.opensdk.openapi.IWXAPIEventHandler;
+import com.tencent.mm.opensdk.openapi.WXAPIFactory;
+
+
+
+public class WXPayEntryActivity extends Activity implements IWXAPIEventHandler {
+
+
+    private IWXAPI api;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        api = WXAPIFactory.createWXAPI(this, "你的APPID");
+        api.handleIntent(getIntent(), this);
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        setIntent(intent);
+        api.handleIntent(intent, this);
+    }
+
+
+    @Override
+    public void onResp(BaseResp resp) {
+
+
+//		if (resp.getType() == ConstantsAPI.COMMAND_PAY_BY_WX) {
+//			AlertDialog.Builder builder = new AlertDialog.Builder(this);
+//			builder.setTitle(R.string.app_tip);
+//			builder.setMessage(getString(R.string.pay_result_callback_msg, String.valueOf(resp.errCode)));
+//		builder.show();
+//		}
+        if (resp.getType() == ConstantsAPI.COMMAND_PAY_BY_WX) {
+            if (resp.errCode == 0) {
+                Toast.makeText(this, "支付成功", Toast.LENGTH_LONG).show();
+            } else {
+                Log.e("java", "onResp: " + resp.errCode);
+                Toast.makeText(this, "支付失败", Toast.LENGTH_LONG).show();
+            }
+            finish();
+        }
+    }
+
+    @Override
+    public void onReq(BaseReq baseReq) {
+
+    }
+}
